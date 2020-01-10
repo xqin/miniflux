993d60692cd10228a95b786df149df786cd5e0bd
xqin <dds_feng@qq.com>
2020-01-11 02:06:25 +0800

    remove unnecessary locale translate

--- locale/locale.go
+++ locale/locale.go
@@ -8,18 +8,6 @@ package locale // import "miniflux.app/locale"
 func AvailableLanguages() map[string]string {
 	return map[string]string{
 		"en_US": "English",
-		"es_ES": "Español",
-		"fr_FR": "Français",
-		"de_DE": "Deutsch",
-		"pl_PL": "Polski",
-		"pt_BR": "Português Brasileiro",
 		"zh_CN": "简体中文",
-		"nl_NL": "Nederlands",
-		"ru_RU": "Русский",
-		"it_IT": "Italiano",
-		"ja_JP": "日本語",
-		"tr_TR": "Türkçe",
-		"el_EL": "Ελληνικά",
-		"fi_FI": "Suomi",
 	}
 }
